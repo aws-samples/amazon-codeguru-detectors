8469658228db936a6c171a768399956164073ab0
aastha <aastjais@amazon.com>
2024-05-14 11:05:19 +0530

    Added missing samples for Go

--- golang/src/detectors/write-pprof-profile-output/rule-write-pprof-profile-output.go
+++ golang/src/detectors/write-pprof-profile-output/rule-write-pprof-profile-output.go
@@ -6,28 +6,24 @@ import(
 	"os"
 )
 
+// {fact rule=best-practices@v1.0 defects=1}
 func dumpGoroutines1(w http.ResponseWriter, r *http.Request, t auth.Token) error {
     if !permission.Check(t, permission.PermDebug) {
         return permission.ErrUnauthorized
     }
 
-    // ruleid: rule-write-pprof-profile-output
+    // Noncompliant:  pprof profile is not removed
     return pprof.Lookup("goroutine").WriteTo(w, 2)
 }
+//{fact}
 
+// {fact rule=write-pprof-profile-output@v1.0 defects=0}
 func dumpGoroutines2(w http.ResponseWriter, r *http.Request, t auth.Token) error {
     if !permission.Check(t, permission.PermDebug) {
         return permission.ErrUnauthorized
     }
 
-    // ruleid: rule-write-pprof-profile-output
-    return pprof.Lookup("goroutine").WriteTo(os.Stdout, 2)
+    // Compliant:  pprof profile is removed
+    return pprof.Lookup("goroutine").WriteTo(os.Stdout, 0)
 }
-func dumpGoroutines3(w http.ResponseWriter, r *http.Request, t auth.Token) error {
-    if !permission.Check(t, permission.PermDebug) {
-        return permission.ErrUnauthorized
-    }
-
-    // ok: rule-write-pprof-profile-output
-    return pprof.Lookup("goroutine").WriteTo(r, 0)
-}
+//{fact}
